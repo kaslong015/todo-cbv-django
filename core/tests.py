be9b28387956087838312dd1ac4c9ec93fab5f15
kaslong <kevinkaslong@gmail.com>
2022-09-08 21:53:25 +0100

    added model test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
-
+from core.models import Task
 # Create your tests here.
+class TestTask(TestCase):
+    def test_task(self):
+        task = Task.objects.create(title="test",description="test",completed=True)
+        self.assertEqual(str(task),'test')
