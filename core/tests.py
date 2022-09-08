f4ec0adecddc9cc4be211565da2fcdf325e6ecf6
kaslong <kevinkaslong@gmail.com>
2022-09-08 22:08:30 +0100

    updated model test

--- core/tests.py
+++ core/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from core.models import Task
+from django.contrib.auth.models import User
 # Create your tests here.
 class TestTask(TestCase):
     def test_task(self):
-        task = Task.objects.create(title="test",description="test",completed=True)
+        task_user = User.objects.create(username="test",password="test123@gh")
+        task = Task.objects.create(user=task_user,title="test",description="test",completed=True)
         self.assertEqual(str(task),'test')
