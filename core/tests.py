27c23adf257cf623d7aa6ecad5a67bca77cc849d
kaslong <kevinkaslong@gmail.com>
2022-09-23 19:19:37 +0100

    new styling

--- core/tests.py
+++ core/tests.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your tests here.
 class TestTask(TestCase):
     def test_task(self):
+        ''' create user instane called task_user and pass it as user ''''
         task_user = User.objects.create(username="test",password="test123@gh")
         task = Task.objects.create(user=task_user,title="test",description="test",completed=True)
         self.assertEqual(str(task),'test')
--- core/views.py
+++ core/views.py
@@ -1,53 +1,54 @@
-from django.shortcuts import render,redirect
-from  django.views.generic import ListView,DetailView,FormView,UpdateView,DeleteView,CreateView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, FormView, UpdateView, DeleteView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
 from core.models import Task
-from core.forms import TaskForm,LoginForm
+from core.forms import TaskForm, LoginForm
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 # Create your views here.
 
+
 class UserLoginView(LoginView):
     redirect_authenticated_user = True
-    template_name ='core/login.html'
+    template_name = 'core/login.html'
 
     def get_redirect_url(self):
         return reverse_lazy('list-task')
 
 
-
-class ListTask(LoginRequiredMixin,ListView):
+class ListTask(LoginRequiredMixin, ListView):
     model = Task
+    template_name = 'core/todo.html'
     context_object_name = 'tasks'
 
 
-class TaskDetail(LoginRequiredMixin,DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_nam = 'task'
 
 
-class CreateTask(LoginRequiredMixin,FormView):
+class CreateTask(LoginRequiredMixin, FormView):
     model = Task
     form_class = TaskForm
-    success_url =  reverse_lazy('list-task')
+    success_url = reverse_lazy('list-task')
     template_name = 'core/form.html'
 
-    def form_valid(self,form):
+    def form_valid(self, form):
         # form.instance.user =self.request.user
         form.save()
         return super().form_valid(form)
 
 
-class UpdateTask(LoginRequiredMixin,UpdateView):
+class UpdateTask(LoginRequiredMixin, UpdateView):
     model = Task
     form_class = TaskForm
     success_url = reverse_lazy('list-task')
     template_name = 'core/form.html'
 
 
-class DeleteTask(LoginRequiredMixin,DeleteView):
+class DeleteTask(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('list-task')
@@ -58,12 +59,11 @@ class CreataUserView(FormView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
 
-    def form_valid(self,form):
+    def form_valid(self, form):
         user = form.save()
         # if user is not None:
         #     return reverse_lazy('login')
-        return super(CreataUserView,self).form_valid(form)
-
+        return super(CreataUserView, self).form_valid(form)
 
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
